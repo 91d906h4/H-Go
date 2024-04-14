d7482e908a92b11224371563830436b4b56a9d50
91d906h4 <fairymanning@gmail.com>
2024-04-14 21:09:44 +0900

    feature: add testing feature
    Add test() function for LS_Trainer to test the accuracy of policy and value network.

--- src/trainer.py
+++ src/trainer.py
@@ -76,6 +76,45 @@ class SL_Trainer:
         # Set model to evaluation mode.
         self.model.eval()
 
+    def test(self, data_reader: DataReader, data_num: int, batch_size: int) -> None:
+        # Set model to evaluation mode.
+        self.model.eval()
+
+        # Set default value.
+        total_acc_policy    = 0
+        total_acc_value     = 0
+
+        for _ in range(data_num // batch_size):
+            # Set default datatypes.
+            policy: torch.Tensor
+            value:  torch.Tensor
+
+            # Get training data batch.
+            testing_batch = data_reader.get_testing_batch(batch_size=batch_size, shuffle=True)
+
+            # Convert data to numpy.array.
+            game_data   = np.array([x[0] for x in testing_batch])
+            step        = np.array([x[1] for x in testing_batch])
+            winner      = np.array([x[2] for x in testing_batch])
+
+            # Convert data to tensor.
+            game_data   = torch.tensor(game_data, dtype=torch.float).to(device=self.device)
+            step        = torch.tensor(step, dtype=torch.long).to(device=self.device)
+            winner      = torch.tensor(winner, dtype=torch.float).to(device=self.device)
+
+            # Get model output.
+            policy, value = self.model(game_data)
+
+            # Calculate accuracy.
+            total_acc_policy += torch.sum(policy.argmax(1) == step).item()
+            total_acc_value += torch.sum(torch.round(value) == winner).item()
+
+        # Print testing information.
+        print(
+            f"Accuracy of policy: {total_acc_policy / data_num * 100:.3f}% | "
+            f"Accuracy of value: {total_acc_value / data_num * 100:.3f}%"
+        )
+
 
 class RL_Trainer:
     def __init__(self) -> None:
