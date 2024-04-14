8ca95e564529903f696a7611607fe9e011386e54
91d906h4 <fairymanning@gmail.com>
2024-04-14 15:35:06 +0900

    feature: add label display mode
    Add 'num' mode and 'char' mode for label displaying.
    'num' mode will show the labels from '0' to 'I'.
    'char' mode will show the labels from 'A' to 'S'.

--- src/gui.py
+++ src/gui.py
@@ -2,17 +2,19 @@ import numpy as np
 
 
 class GUI:
-    def __init__(self, size: int=19) -> None:
+    def __init__(self, size: int=19, label_mode: str="num") -> None:
         """GUI class
 
         A class for displaying the game board.
 
         Args:
             size (int): The size of the board.
+            label_mode (str): The mode of the label. (default: "num")
 
         """
 
         self.size = size
+        self.label_mode = label_mode
 
     def display(self, state: list, move: int) -> None:
         """display public method
@@ -26,8 +28,14 @@ class GUI:
         """
 
         # Set the labels.
-        labels = "0123456789ABCDEFGHI"
+        labels = {
+            "num": "0123456789ABCDEFGHI",
+            "char": "ABCDEFGHIJKLMNOPQRS",
+        }
         stone = {0: "·", 1: "O", -1: "X"}
+
+        # Set label mode.
+        labels = labels[self.label_mode]
 
         # Calculate the last move position.
         last_move_row = move // self.size
